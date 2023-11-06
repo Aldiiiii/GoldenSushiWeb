93d1b50e889586ada40f294f97e1e2f2a0dcd8d2
Aldiiiii <amrinaldi.rizki28@gmail.com>
2023-11-06 10:39:02 +0700

    setup for deploy done

--- client-user/golden-sushi/src/components/Carousel.jsx
+++ client-user/golden-sushi/src/components/Carousel.jsx
@@ -5,22 +5,22 @@ function CarouselFadeExample() {
     <div id="landing">
     <Carousel fade>
       <Carousel.Item >
-        <img src="./src/assets/images/carousel-sushi1.jpg" style={{height: "100vh", width: "100vw"}} alt="" />
+        <img src="https://png.pngtree.com/background/20230520/original/pngtree-sushi-pictures-on-your-phone-picture-image_2670262.jpg" style={{height: "100vh", width: "100vw"}} alt="" />
         {/* <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption> */}
       </Carousel.Item>
       <Carousel.Item>
-      <img src="./src/assets/images/carousel-sushi2.jpg" style={{height: "100vh", width: "100vw"}} alt="" />
+      <img src="https://images.deliveryhero.io/image/fd-sg/LH/zn4e-hero.jpg" style={{height: "100vh", width: "100vw"}} alt="" />
 
       </Carousel.Item>
       <Carousel.Item>
-      <img src="./src/assets/images/carousel-sushi3.jpg" style={{height: "100vh", width: "100vw"}} alt="" />
+      <img src="https://wallpaperaccess.com/full/412959.jpg" style={{height: "100vh", width: "100vw"}} alt="" />
 
       </Carousel.Item>
       <Carousel.Item>
-        <img src="./src/assets/images/carousel-sushi4.jpg" style={{height: "100vh", width: "100vw"}} alt="" />
+        <img src="https://i0.wp.com/www.mysweetescapediary.com/wp-content/uploads/2018/01/genki-sushi-2.jpg?fit=4608%2C3456&ssl=1" style={{height: "100vh", width: "100vw"}} alt="" />
 
       </Carousel.Item>
     </Carousel>
